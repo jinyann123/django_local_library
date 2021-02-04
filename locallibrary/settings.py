f8bee4530efb5ef322837c8a72b421c59c64c4ce
jinyan.ni <jinyan.ni@citrix.com>
2021-02-04 19:23:23 +0800

    commit

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'xv^8o^)si^f=pfo$=e!tz&#&-g3@#e
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+# Set hosts to allow any app on Heroku and the local testing URL
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
